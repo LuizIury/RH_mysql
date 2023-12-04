77eb583481c4c5d22b09887eb25c0391af98bf2f
Luiz Iury <luiziury_ios@hotmail.com>
2023-12-04 13:51:33 -0300

    Terceiro exercício MySQL

---
+++ Exercícios MySQL/Exe03.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE db_RH;
+
+USE db_RH;
+
+CREATE TABLE tb_colaboradores(
+ id BIGINT AUTO_INCREMENT,
+ nome VARCHAR(255) NOT NULL,
+ datanascimento DATE,
+ cpf VARCHAR(14),
+ salario DECIMAL(7,2) NOT NULL,
+ funcao VARCHAR(25),
+ PRIMARY KEY (id)
+ );
+
+INSERT INTO tb_colaboradores(nome, datanascimento, cpf, salario, funcao)
+VALUES ("João", "2001-01-24", 34528467598, 1800, "Estagiário"),
+("Maria", "1995-12-05", 84710948630, 2300, "Recrutadora"),
+("Pedro", "2001-01-24", 34586233415, 5000, "Chefe"),
+("Lara", "2001-01-24", 98723645692, 1300, "Aux. Escritório"),
+("Estela", "2001-01-24", 85738594812, 2050, "Desenvolvedora Jr.");
+
+
+UPDATE tb_colaboradores SET nome = "Laysla" WHERE Id = 2;
+
+SELECT * FROM tb_colaboradores;
+
+
+
+
